89d87eff104cbd47e8ad904adbe131e09eecf518
Russel Ndip <ndiprussel@gmail.com>
2020-08-31 11:58:14 -0500

    json objects added

--- webAPI/__init__.py
+++ webAPI/__init__.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 import markdown
 import os
+import json, io
 
 from flask import Flask, jsonify
 from flask_restful import Resource, Api, reqparse
 
 # Create the application instance
 app = Flask(__name__)
+with open(devices.json) as f
+    devices = json.load(f)
+with open(users.json) as u
+    users = json.load(u)
 
 #create the API
 api = Api(app)
@@ -22,8 +27,13 @@ def index():
         #Convert to HTML
         return markdown.markdown(content)
 
+@app.route('/user', methods = ['GET'])
+def get_all_users():
+    return ''
+@app.route('/user/<')
 class DeviceList(Resource)
     def get(self):
+
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
